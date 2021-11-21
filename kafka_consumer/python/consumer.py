89c8cdc5c9ab85f43fe2e5fefdeb6d73a6ba1280
sia14 <sia14.sfu.ca>
2021-11-20 23:54:44 -0800

    send numpy arrays through the pipe.

--- kafka_consumer/python/consumer.py
+++ kafka_consumer/python/consumer.py
@@ -1,62 +1,69 @@
-
 import os, sys
 from kafka import KafkaConsumer, KafkaProducer
-import datetime as dt
-# import ast
+from datetime import datetime
+from threading import Thread
+import queue
+import numpy as np
 
-sasl_mechanism = 'PLAIN'
-security_protocol = 'SASL_PLAINTEXT'
+now = lambda : str(datetime.now())
 
-DEFAULT_TIMEOUT=5000
-SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 300))
+class MsgReceiver(Thread):
+    def __init__(self) -> None:
+        super(MsgReceiver,self).__init__(name='MsgReceiverThread')
+        self.init()
+        print(f"{now()} Setting up Kafka consumer at {self.KAFKA_BROKER_URL}")
+        self.consumer = KafkaConsumer(self.TOPIC_NAME, bootstrap_servers=self.KAFKA_BROKER_URL,
+                                sasl_plain_username = self.KAFKA_USERNAME,
+                                sasl_plain_password = self.KAFKA_PASSWORD,
+                                security_protocol = self.security_protocol,
+                                sasl_mechanism = self.sasl_mechanism)
+        self.q = queue.Queue()
 
-# Kafka producer
-KAFKA_BROKER_URL = (
-    os.environ.get("KAFKA_BROKER_URL")
-    if os.environ.get("KAFKA_BROKER_URL")
-    else "localhost:9092"
-)
-TOPIC_NAME = (
-    os.environ.get("TOPIC_NAME") if os.environ.get("TOPIC_NAME") else "test"
-)
+    def init(self, verbose=None):
+        self.sasl_mechanism = 'PLAIN'
+        self.security_protocol = 'SASL_PLAINTEXT'
 
-KAFKA_USERNAME = (
-    os.environ.get("KAFKA_USERNAME") if os.environ.get("KAFKA_USERNAME") else ""
-)
-KAFKA_PASSWORD = (
-    os.environ.get("KAFKA_PASSWORD") if os.environ.get("KAFKA_PASSWORD") else ""
-)
+        self.DEFAULT_TIMEOUT=5000
+        self.SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 300))
+        self.MAX_SIZE = 104857600
+
+        self.KAFKA_BROKER_URL = (
+            os.environ.get("KAFKA_BROKER_URL")
+            if os.environ.get("KAFKA_BROKER_URL")
+            else "localhost:9092"
+        )
+        self.TOPIC_NAME = (
+            os.environ.get("TOPIC_NAME") if os.environ.get("TOPIC_NAME") else "test"
+        )
+
+        self.KAFKA_USERNAME = (
+            os.environ.get("KAFKA_USERNAME") if os.environ.get("KAFKA_USERNAME") else ""
+        )
+        self.KAFKA_PASSWORD = (
+            os.environ.get("KAFKA_PASSWORD") if os.environ.get("KAFKA_PASSWORD") else ""
+        )
+
+    def getNextMsg(self, block=True, timeout=None) -> np.ndarray:
+        return self.q.get(block, timeout)
+
+    def run(self):
+        print(f'{now()} Waiting for msg...')
+        for msg in self.consumer:
+            # msg = msg.value.decode('utf-8')
+            received = np.frombuffer(msg.value, dtype=float)
+            #gonna have to agree on a size or send it too.
+            received = np.reshape(received, (3,3))
+            print(f"{now()} Message received a {received.shape} array")
+            self.q.put(received)
 
 if __name__ == "__main__":
-    
-    print(f"{str(dt.datetime.now())} Setting up Kafka consumer at {KAFKA_BROKER_URL}")
-    consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=KAFKA_BROKER_URL,
-                            sasl_plain_username = KAFKA_USERNAME,
-                            sasl_plain_password = KAFKA_PASSWORD,
-                            security_protocol = security_protocol,
-                            sasl_mechanism = sasl_mechanism)
+    #start receiver in a new thread
+    receiver = MsgReceiver()
+    receiver.start()
 
-    # producer = KafkaProducer(bootstrap_servers = KAFKA_BROKER_URL,
-    #                      sasl_plain_username = KAFKA_USERNAME,
-    #                      sasl_plain_password = KAFKA_PASSWORD,
-    #                      security_protocol = security_protocol,
-    #                      sasl_mechanism = sasl_mechanism,
-    #                     #  value_serializer=lambda x: x.encode("utf8"),
-    #                      api_version=(0, 11, 5),
-    #                      max_request_size=104857600,
-    #                      retries=5,
-    #                      max_block_ms=DEFAULT_TIMEOUT)
-    print('Waiting for msg...')
-    for msg in consumer:
-        # print('got one!')
-        msg = msg.value.decode('utf-8')
-        print(f"{str(dt.datetime.now())} Message received : {msg}")
-
-        # data = ast.literal_eval(msg)
-        
-        # print("Sending it to Sink")
-        # dic = json.dumps(dic)
-        # producer.send(SINK_TOPIC, value = dic)
-        # print("Sent")
+    #get messages received
+    while(True):
+        newdata = receiver.getNextMsg(block=True) #blocking or non blocking
+        print(f'{now()} Client received: {newdata}')
 
     print("Bye-Bye")
--- kafka_producer/producer.py
+++ kafka_producer/producer.py
@@ -1,67 +1,73 @@
 import sys
-import configparser
-import json
 import os
 import time
 from datetime import datetime, timedelta
 from kafka import KafkaProducer
 import numpy as np
 
+now = lambda : str(datetime.now())
 
-sasl_mechanism = 'PLAIN'
-security_protocol = 'SASL_PLAINTEXT'
-
-DEFAULT_TIMEOUT=5000
-SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 300))
-
-# Kafka producer
-KAFKA_BROKER_URL = (
-    os.environ.get("KAFKA_BROKER_URL")
-    if os.environ.get("KAFKA_BROKER_URL")
-    else "localhost:9092"
-)
-TOPIC_NAME = (
-    os.environ.get("TOPIC_NAME") if os.environ.get("TOPIC_NAME") else "test"
-)
-
-KAFKA_USERNAME = (
-    os.environ.get("KAFKA_USERNAME") if os.environ.get("KAFKA_USERNAME") else ""
-)
-KAFKA_PASSWORD = (
-    os.environ.get("KAFKA_PASSWORD") if os.environ.get("KAFKA_PASSWORD") else ""
-)
-
-
-class producer:
-
+class MsgSender:
     def __init__(self):
-        
-        self.producer = KafkaProducer(bootstrap_servers = KAFKA_BROKER_URL,
-                         sasl_plain_username = KAFKA_USERNAME,
-                         sasl_plain_password = KAFKA_PASSWORD,
-                         security_protocol = security_protocol,
-                         sasl_mechanism = sasl_mechanism,
-                         value_serializer=lambda x: x.encode("utf8"),
+        self.init()
+        self.producer = KafkaProducer(bootstrap_servers = self.KAFKA_BROKER_URL,
+                         sasl_plain_username = self.KAFKA_USERNAME,
+                         sasl_plain_password = self.KAFKA_PASSWORD,
+                         security_protocol = self.security_protocol,
+                         sasl_mechanism = self.sasl_mechanism,
+                        #  value_serializer=lambda x: x.encode("utf8"),
                          api_version=(0, 11, 5),
-                         max_request_size=104857600,
+                         max_request_size=self.MAX_SIZE,
                          retries=5,
-                         max_block_ms=DEFAULT_TIMEOUT)
+                         max_block_ms=self.DEFAULT_TIMEOUT)
 
+
+    def sendMsg(self, data : np.ndarray):
+        if(data is None or data.size <=0):
+            print(f"{now} Cant continue with empty data")
+            return
+        print(f"{now()} Sending tensor witg {data.size} elements")
+        msg = data.flatten().tobytes()
+        try:
+            print(f'{now()} Sending kafka msg to {self.TOPIC_NAME} topic')
+            self.producer.send(self.TOPIC_NAME, value=msg)
+            self.producer.flush()
+            print(f'{now()} Done sending')
+        except:
+            print(f"{now()} Unexpected error:", sys.exc_info())
     def run(self):
         date_to = datetime.utcnow()
-        msg = "hello world"
-        print(f'Sending kafka msg to {TOPIC_NAME}')
+        msg = np.random.randint(0, 100, (3,3))
         while(True):
-            try:
-                self.producer.send(TOPIC_NAME, value=msg)
-                self.producer.flush()
-                print('Done')
-            except:
-                print("Unexpected error:", sys.exc_info())
+            self.sendMsg(msg)    
             time.sleep(5)
+    def init(self):
+        self.sasl_mechanism = 'PLAIN'
+        self.security_protocol = 'SASL_PLAINTEXT'
+
+        self.DEFAULT_TIMEOUT=5000
+        self.SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 300))
+        self.MAX_SIZE = 104857600
+
+        # Kafka producer
+        self.KAFKA_BROKER_URL = (
+            os.environ.get("KAFKA_BROKER_URL")
+            if os.environ.get("KAFKA_BROKER_URL")
+            else "localhost:9092"
+        )
+        self.TOPIC_NAME = (
+            os.environ.get("TOPIC_NAME") if os.environ.get("TOPIC_NAME") else "test"
+        )
+
+        self.KAFKA_USERNAME = (
+            os.environ.get("KAFKA_USERNAME") if os.environ.get("KAFKA_USERNAME") else ""
+        )
+        self.KAFKA_PASSWORD = (
+            os.environ.get("KAFKA_PASSWORD") if os.environ.get("KAFKA_PASSWORD") else ""
+        )    
 
 if __name__ == "__main__":
-    print(f"Starting producer for broker at {KAFKA_BROKER_URL}")
-    service = producer()
+    service = MsgSender()
+    print(f"{now()} Starting producer test at {service.KAFKA_BROKER_URL}")
     service.run()
     # time.sleep(10) #adding sleep here just to be able to see the logs after running.
