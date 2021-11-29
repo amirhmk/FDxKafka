b7bf5230736f137ba0b0e2e91a32232399cf17c8
Amir Mousavi <amirh@collisure.com>
2021-11-28 19:01:33 -0800

    Matches deps with flwr

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys, os
+import subprocess
 sys.path.insert(0, os.getcwd())
 
 import fd_engine.client
 
 def handler(request):
+    # subprocess.call(". setup.sh", shell=True, executable='/bin/bash')
     fd_engine.client.main(2)
 
 if __name__ == "__main__":
