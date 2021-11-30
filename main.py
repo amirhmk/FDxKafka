aa662b94ed193240cedcc9ed636b6d2bcf455f22
sia14 <sia14.sfu.ca>
2021-11-30 11:37:44 -0800

    function parameters

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import time
 sys.path.insert(0, os.getcwd())
 
 import fd_engine.client
-import fd_engine.cifar_numpy_test
+# import fd_engine.cifar_numpy_test
 
 def handler(request):
     start_time = time.time()
     # subprocess.call(". setup.sh", shell=True, executable='/bin/bash')
-    broker = "10.138.0.6:9092"
-    if request.broker is not None:
+    print(f"request: {request}")
+    print(f"request broker: {request.broker}")
+    broker = "34.105.38.178:9091"
+    if request is not None and request.broker is not None:
       broker = request.broker
     
-    fd_engine.cifar_numpy_test.main(broker)
+    fd_engine.client.main(2,broker)
+    # fd_engine.cifar_numpy_test.main(broker)
     end_time = time.time()
     duration = f"{end_time - start_time} Sec"
     print("Duration: ", duration)
