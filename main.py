18e84c2af1fd03c52970cc2d5de5b78025f7fd86
Amir Mousavi <amousavi@me.com>
2021-11-30 08:12:13 +0000

    GCP functions

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import sys, os
-import subprocess
+import time
 sys.path.insert(0, os.getcwd())
 
 import fd_engine.client
+import fd_engine.cifar_numpy_test
 
 def handler(request):
+    start_time = time.time()
     # subprocess.call(". setup.sh", shell=True, executable='/bin/bash')
-    fd_engine.client.main(2)
+    # fd_engine.client.main(2)
+    kafka_server = "10.138.0.6:9092"
+    fd_engine.cifar_numpy_test.main(kafka_server)
+    end_time = time.time()
+    duration = f"{end_time - start_time} Sec"
+    print("Duration: ", duration)
+    return duration
 
 if __name__ == "__main__":
     handler("")
