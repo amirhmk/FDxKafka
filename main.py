15fb7b7eb26dad5265e7039b6f089583b76826ac
sia14 <sia14.sfu.ca>
2021-11-30 11:39:00 -0800

    function changes

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ def handler(request):
     start_time = time.time()
     # subprocess.call(". setup.sh", shell=True, executable='/bin/bash')
     print(f"request: {request}")
-    print(f"request broker: {request.broker}")
     broker = "34.105.38.178:9091"
     if request is not None and request.broker is not None:
-      broker = request.broker
-    
+        broker = request.broker
+        print(f"request broker: {request.broker}")
     fd_engine.client.main(2,broker)
     # fd_engine.cifar_numpy_test.main(broker)
     end_time = time.time()
