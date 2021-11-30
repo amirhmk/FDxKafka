cef8b54ece6bc3cab4cdb4587285b010ff78e7ac
Amir Mousavi <amirh@collisure.com>
2021-11-30 15:05:45 -0800

    Cloud function handles args

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ import time
 sys.path.insert(0, os.getcwd())
 
 import fd_engine.client
-# import fd_engine.cifar_numpy_test
 
 def handler(request):
     start_time = time.time()
-    # subprocess.call(". setup.sh", shell=True, executable='/bin/bash')
-    print(f"request: {request}")
-    broker = "34.105.38.178:9091"
-    if request is not None and request.broker is not None:
-        broker = request.broker
-        print(f"request broker: {request.broker}")
-    fd_engine.client.main(2,broker)
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+    print("request_json", request_json)
+
+    if request_json and 'broker' in request_json:
+        broker = request_json['broker']
+    elif request_args and 'broker' in request_args:
+        broker = request_args['broker']
+    else:
+        broker = "34.105.38.178:9091"
+    print(f"request broker: {broker}")
+    fd_engine.client.main(2, broker)
     # fd_engine.cifar_numpy_test.main(broker)
     end_time = time.time()
-    duration = f"{end_time - start_time} Sec"
-    print("Duration: ", duration)
+    duration = end_time - start_time
+    print(f"Duration: {duration} Sec")
     return duration
 
 if __name__ == "__main__":
